43e69e18cc28d3404a4d25f24ac1d5ad8a9f5d56
jpps <joao.pedro@labsit.io>
2024-04-10 18:05:53 -0300

    tirando chave e ajustados codigos

--- ffxiv.py
+++ ffxiv.py
@@ -8,21 +8,30 @@ import pyxivapi
 from Item import Item
 
 from pyxivapi.models import Filter, Sort
-
-
-async def getItemByName(name):
-    client = pyxivapi.XIVAPIClient(api_key=os.environ['xivapi_key'])
-
+client = pyxivapi.XIVAPIClient(api_key=os.environ['xivapi_key'])
+async def getItemByName(name):    
     # Item search with paging
-    itemResultado = await client.index_search(
+    item = await client.index_search(
         name=name,
-        indexes=["Recipe"],
+        indexes=["Recipe","Item"],
         columns=["ID","Name","Icon","UrlType"],
         page=0,
         per_page=10
     )
     await client.session.close()
-    return itemResultado['Results'][0]
+    print(item['Results'])
+    return item['Results'][0]
+
+async def getItemById(id):
+    
+    item = await client.index_by_id(
+        index=id,
+        columns=["ID","Name","Icon","UrlType"]
+    )
+    await client.session.close()
+    return item['Results'][0]
+
+
 
 
 if __name__ == '__main__':   
@@ -31,6 +40,7 @@ if __name__ == '__main__':
     #logging.basicConfig(level=logging.INFO, format='%(message)s', datefmt='%H:%M')
     loop = asyncio.get_event_loop()
     item = Item(loop.run_until_complete(getItemByName("Indagator's Helmet of Gathering")))
-    
+    print(f"Encontrado item {item.ID} - {item.Name}")
+
    
     
