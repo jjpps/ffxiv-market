ac0cca5d49b4d9e86a9f7e0f176e04370714237f
jpps-casa <joaopedro-ps@hotmail.com>
2024-04-13 19:55:01 -0300

    escrevendo no excel

--- Ingredient.py
+++ Ingredient.py
@@ -1,5 +1,6 @@
 class Ingredient:
-    def __init__(self, id,name,amount):
+    def __init__(self, id,name,amount,icon=None):
         self.id=id
         self.Name =name
         self.Amount =amount      
+        self.Icon = icon
--- ffxiv.py
+++ ffxiv.py
@@ -4,6 +4,7 @@ import logging
 import json
 import aiohttp
 import pyxivapi
+import csv
 #from dotenv import load_dotenv
 from Item import Item
 from RecipeClass import RecipeClass
@@ -60,19 +61,20 @@ def recursiveSearch(ingredient:Ingredient):
             for x in range(10):
                 ingredient = receitaIngrediantes[f"ItemIngredient{x}"]
                 amountIngredient = receitaIngrediantes[f"AmountIngredient{x}"]
+                Icon = receitaIngrediantes[f"Icon"]
                 if(ingredient and amountIngredient):
                     #print(f"Ingredient {ingredient['ID']} name {ingredient['Name']}")
-                    ingredientList.append(Ingredient(ingredient['ID'],ingredient['Name'] ,amountIngredient))
+                    ingredientList.append(Ingredient(ingredient['ID'],ingredient['Name'] ,amountIngredient,Icon))
             return ingredientList
 
         
 
 
 
-def main():
+def main(item):
     ItemFinal = []
     loop = asyncio.get_event_loop()
-    itemToCraft = loop.run_until_complete(getItemByName("Item","Indagator's Doublet Vest of Gathering"))
+    itemToCraft = loop.run_until_complete(getItemByName("Item",item))
     #print(f"Encontrado {itemToCraft["UrlType"]} {itemToCraft["ID"]} - {itemToCraft["Name"]}")
 
     recipeClass = loop.run_until_complete(getItemById("Recipe",itemToCraft["ID"]))    
@@ -94,10 +96,12 @@ def main():
     for i in ingredientList:        
         ItemFinal.append(recursiveSearch(i))
     
-    for item in ItemFinal:
-        if(item): 
-            for i in item:
-                print(f"Name do Item {i.Name} qtd: {i.Amount}")
+    # for item in ItemFinal:
+    #     if(item): 
+    #         for i in item:
+    #             print(f"Name do Item {i.Name} qtd: {i.Amount} Icon:{i.Icon}")
+    
+    return ItemFinal
         
         
 
@@ -107,7 +111,34 @@ def main():
 
 
 if __name__ == '__main__':
-    main()       
+    #Indagator's Doublet Vest of Gathering
+    itemList =[]    
+    itemToCsv=[]
+    haveMore = True
+    while (haveMore):
+        var = str(input("Item to Search: "))
+        itemList.append(var)
+        haveMore = True if input("Continue [Y/N] ?")=="Y" else False
+    for i in itemList:
+        itemToCsv.append(main(i))
+
+    with open('Crafting.csv', 'w', newline='',) as file:
+        writer = csv.writer(file)
+        field = ["Name", "Amount", "Icon"]
+        
+        writer.writerow(field)
+        for item in itemToCsv:
+            if(item): 
+                for i in item:
+                    if(i is not None):
+                        for i2 in i:
+                            writer.writerow([i2.Name, i2.Amount, i2.Icon])        
+    
+
+
+
+
+    
     
             
 
